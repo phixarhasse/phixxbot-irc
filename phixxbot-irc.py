d97bc310c646b9c555354cbee9644c4a2d70f4e9
Joel Andersson <joeland@student.chalmers.se>
2021-04-26 22:46:22 +0100

    Move token variable. Add error handling.

--- phixxbot-irc.py
+++ phixxbot-irc.py
@@ -1,63 +1,78 @@
 import requests
 import miniirc
 import time
+import sys
+from datetime import datetime
 
 # Constants
 CLIENT_SECRET = ""
 CLIENT_ID = ""
-ACCESS_TOKEN = ""
 TWITCH_API_URL = "https://api.twitch.tv/helix/streams"
+TWITCH_TOKEN_API = "https://id.twitch.tv/oauth2/token"
 CHANNELS = {"djphixx" : False,
             "drsldr"  : False}
 
-# Set up IRC connection and API request
+# Set up IRC connection
 irc = miniirc.IRC('irc.dtek.se', 6697, 'PhixxBot', ['#dtek'], verify_ssl=False)
-headers = {"Client-Id": CLIENT_ID, "Authorization": "Bearer " + ACCESS_TOKEN}
+
 
 def main():
+    token = authenticate()
+    if(token == ""):
+        sys.exit("Could not authenticate to Twitch. Exiting.")
+
+    headers = {"Client-Id": CLIENT_ID, "Authorization": "Bearer " + token}
+    irc.send('PRIVMSG', '#dtek', "I'm alive for realz now!")
     while(True):
-        for channel in CHANNELS:
-            parameters = {"user_login": channel}
-            try:
-                response = requests.request("GET", TWITCH_API_URL, params=parameters, headers=headers)
-                print("Twitch repsonse:", response.status_code)
-                responseJson = response.json()
-            except Exception as e:
-                print(e)
-                time.sleep(60)
-                continue
+        break
+        try:
+            for channel in CHANNELS:
+                parameters = {"user_login": channel}
+                try:
+                    response = requests.request("GET", TWITCH_API_URL, params=parameters, headers=headers)
+                    print("Twitch repsonse:", response.status_code)
+                    responseJson = response.json()
+                except Exception as e:
+                    print(e)
+                    time.sleep(60)
+                    continue
 
-            # Channel has gone live
-            if(response.ok and (len(responseJson["data"]) > 0) and not CHANNELS[channel]):
-                CHANNELS[channel] = True
-                print("Channel twitch.tv/" + channel + " is live with " + responseJson["data"][0]["game_name"] + "!")
-                irc.send('PRIVMSG', '#dtek', "Channel twitch.tv/" + channel + " is live with " + responseJson["data"][0]["game_name"] + "!")
+                # Channel has gone live
+                if(response.ok and (len(responseJson["data"]) > 0) and not CHANNELS[channel]):
+                    CHANNELS[channel] = True
+                    print("Channel twitch.tv/" + channel + " is live with " + responseJson["data"][0]["game_name"] + "!")
+                    irc.send('PRIVMSG', '#dtek', "Channel twitch.tv/" + channel + " is live with " + responseJson["data"][0]["game_name"] + "!")
 
-            # Channel was live and gone offline
-            elif(response.ok and CHANNELS[channel] and len(responseJson["data"]) == 0):
-                print("Channel twitch.tv/" + channel + " has gone offline. See you next time!")
-                #irc.send('PRIVMSG', '#dtek', "Channel twitch.tv/" + channel + " has gone offline. See you next time!")
-                CHANNELS[channel] = False
+                # Channel was live and gone offline
+                elif(response.ok and CHANNELS[channel] and len(responseJson["data"]) == 0):
+                    print("Channel twitch.tv/" + channel + " has gone offline. See you next time!")
+                    CHANNELS[channel] = False
 
-            # Twitch error
-            elif(not response.ok):
-                print("Twitch error:", response.text)
+                # Twitch error
+                elif(not response.ok):
+                    print("Twitch error:", response.text)
 
-            # None of the selected channels are live
-            else:
-                print("Channel twitch.tv/" + channel + " is not live. *cricket noise*")
-        time.sleep(45)
+                # None of the selected channels are live
+                else:
+                    print("Channel twitch.tv/" + channel + " is not live. *cricket noise*")
+            time.sleep(45)
+        except KeyboardInterrupt:
+            sys.exit("Keyboard interruption detected. Exiting.")
 
 def authenticate():
+
     parameters={ "client_id" : CLIENT_ID, "client_secret":  CLIENT_SECRET, "grant_type": "client_credentials" }
     try:
-        response = requests.request("POST", "https://id.twitch.tv/oauth2/token", params=parameters)
+        response = requests.request("POST", TWITCH_TOKEN_API, params=parameters)
     except Exception as e:
                 print(e)
-                return
+                return ""
     if(response.status_code == 200):
         json = response.json()
-        ACCESS_TOKEN = json["access_token"]
+        return json["access_token"]
+    else:
+        print("Error during authentication: " + response.status_code + " " + response.reason)
+        return ""
 
 
 
